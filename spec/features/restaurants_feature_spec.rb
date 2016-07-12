f7b4cced923eb36548bea3df66d2cbb1c8b51bb5
tobywilkins <tobywilkinsuK@hotmail.co.uk>
2016-07-12 10:33:36 +0100

    up to challenge 14

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -30,6 +30,15 @@ feature 'restaurants' do
       expect(page).to have_content 'KFC'
       expect(current_path).to eq '/restaurants'
     end
+
+    scenario 'an invalid restaurant name (short) raises error' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'kf'
+      click_button 'Create Restaurant'
+      expect(page).not_to have_css 'h2', text: 'kf'
+      expect(page).to have_content 'error'
+    end
   end
 
   context 'viewing restaurants' do
@@ -74,5 +83,7 @@ feature 'restaurants' do
     end
 
   end
+
+
 
 end
